5868a490e612c19984650e338ad632c1e5e248ab
kimjeju <h.hwi9367@gmail.com>
2024-03-11 22:09:14 +0900

    gltf 로딩

--- r3f_ecommers/src/components/three/ShowRoom.tsx
+++ r3f_ecommers/src/components/three/ShowRoom.tsx
@@ -1,15 +1,17 @@
 import * as THREE from 'three'
-import { FBXLoader, OBJLoader,  } from 'three/examples/jsm/Addons.js'
+import { FBXLoader, OBJLoader, GLTFLoader } from 'three/examples/jsm/Addons.js'
 import { useLoader } from '@react-three/fiber'
 
 export default function ShowRoom(){
 
-    const obj = useLoader(OBJLoader, './models/custom.obj');
-    const fbx = useLoader(FBXLoader, './models/custom.fbx');
+    // const obj = useLoader(OBJLoader, './models/custom.obj');
+    // const fbx = useLoader(FBXLoader, './models/custom.fbx');
+    const gltf = useLoader(GLTFLoader, "./models/custom.glb")
 
     return(
         <>  
-            <primitive object={obj} />
+            {/* <primitive object={obj} />  */}
+            <primitive object={gltf.scene} />
             {/* <primitive object={fbx} /> */}
             {/* <mesh 
                 rotation={[
