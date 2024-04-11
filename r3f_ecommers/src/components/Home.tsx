1cce54ef80bf4fc448c2021eb6e3dcd76d802b70
kimjeju <h.hwi9367@gmail.com>
2024-04-11 20:49:35 +0900

    MUI 메뉴바 추가

--- r3f_ecommers/src/components/Home.tsx
+++ r3f_ecommers/src/components/Home.tsx
@@ -1,15 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import ShowRoom from "@components/three/ShowRoom";
 import { OrbitControls } from "@react-three/drei";
+import ResponsiveAppBar from "./AppBar";
 
 export default function Home(){
 
     return(
         <>
+            <ResponsiveAppBar />
             <Canvas>   
                 {/* <OrbitControls />  */}
                 {/* <axesHelper args={[5]} />
                 <gridHelper /> */}
+                <color attach={'background'} args={['#b7f2f1']}/>   
                 <ShowRoom />
             </Canvas>
         </>
